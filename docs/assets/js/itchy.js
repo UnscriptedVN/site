3a2206a4e6fe5731d3a8264c7b2a8c2c60681296
Marquis Kurt <software@marquiskurt.net>
2021-10-22 10:46:43 -0400

    [site] Fetch posts from Itch, fix flexbox issues

--- docs/assets/js/itchy.js
+++ docs/assets/js/itchy.js
@@ -1,6 +1,40 @@
 function linkGameButton() {
-  Itch.attachBuyButton(document.getElementById("itch_download"), {
-    user: "marquiskurt",
-    game: "unscripted",
-  });
+    Itch.attachBuyButton(document.getElementById("itch_download"), {
+        user: "marquiskurt",
+        game: "unscripted",
+    });
 }
+
+const _setupItch = () => {
+    let parser = new RSSParser();
+
+    return {
+        feed: [],
+        sources: [
+            "https://marquiskurt.itch.io/unscripted/devlog.rss"
+        ],
+
+        get (source) {
+            parser.parseURL(source, (err, feed) => {
+                if (err) console.error(err);
+                feed.items.forEach(entry => this.addToFeed(entry));
+            });
+        },
+
+        addToFeed(item) {
+            if (this.feed.length >= 3) return;
+            this.feed.push({
+                author: item.author,
+                title: item.title,
+                link: item.link,
+                date: new Date(item.pubDate),
+                excerpt: item.contentSnippet
+            })
+        },
+
+        init() { 
+            this.sources.forEach(source => this.get(source))
+            console.log('Initialized feed.') 
+        }
+    }
+};
